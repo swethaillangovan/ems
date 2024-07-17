676d8315400e77faa88d041a814897c6b825d005
Swetha Illangovan <swetha.illangovan@agilysys.com>
2024-07-17 21:57:09 +0530

    intial commit

---
+++ api/EmployeeManagementSystem/Controllers/EmployeeController.cs
@@ -0,0 +1,25 @@
+﻿using EmployeeManagementSystem.Model;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EmployeeManagementSystem.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EmployeeController : ControllerBase
+    {
+        /// <summary>
+        /// Api to get all the employee details
+        /// </summary>
+        /// <returns></returns>
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Employee>>> GetEmployees()
+        {
+            var employees = new List<Employee>()
+            {
+                new Employee { Id = 1, Name = "Test" }
+            };
+            return Ok(employees);
+        }
+    }
+}
---
+++ api/EmployeeManagementSystem/Model/Employee.cs
@@ -0,0 +1,18 @@
+﻿namespace EmployeeManagementSystem.Model
+{
+    /// <summary>
+    /// Model for Employee
+    /// </summary>
+    public class Employee
+    {
+        /// <summary>
+        /// Id of the Employee
+        /// </summary>
+        public int Id { get; set; }
+
+        /// <summary>
+        /// EmployeeName: Name of the employee, it includes firstname and lastname
+        /// </summary>
+        public string Name { get; set; }
+    }
+}
